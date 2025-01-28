6f792ee96cdb4c6e7ec7adfc10fde8776d30c0ce
SablezubRabbit <yurinv@mail.ru>
2025-01-28 16:48:53 +0300

    Add redirect

--- women/views.py
+++ women/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse, HttpResponseNotFound, Http404
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
 from django.views.defaults import page_not_found
 
 
@@ -17,8 +18,9 @@ def categories_by_slug(request, cat_slug):
 
 def archive(request, year):
     if year > 2024:
-        raise Http404()
-    
+        uri = reverse('cats', args=('music'))
+        return redirect(uri)
+
     return HttpResponse(f'<h1>Архив по годам</h1><p>{year}</p>')
 
 def page_not_found(request, exception):
