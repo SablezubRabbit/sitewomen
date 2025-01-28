f5da40d49d5fd5542f57008d3fa6e494b73edc1a
SablezubRabbit <yurinv@mail.ru>
2025-01-28 15:14:19 +0300

    Add page 404

--- women/views.py
+++ women/views.py
@@ -1,5 +1,6 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.shortcuts import render
+from django.views.defaults import page_not_found
 
 
 def index(request):
@@ -15,4 +16,10 @@ def categories_by_slug(request, cat_slug):
 
 
 def archive(request, year):
+    if year > 2024:
+        raise Http404()
+    
     return HttpResponse(f'<h1>Архив по годам</h1><p>{year}</p>')
+
+def page_not_found(request, exception):
+    return HttpResponseNotFound('<h2>Страница не найдена</h2>')
