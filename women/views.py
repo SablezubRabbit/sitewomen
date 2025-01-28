4589d48785bd364ce8912bf23e645679995b819d
SablezubRabbit <yurinv@mail.ru>
2025-01-28 23:45:53 +0300

    End "redirect"

--- women/views.py
+++ women/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse, HttpResponseNotFound, Http404
+from django.http import HttpResponse, HttpResponseNotFound, Http404, HttpResponseRedirect, HttpResponsePermanentRedirect
 from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.views.defaults import page_not_found
@@ -18,8 +18,9 @@ def categories_by_slug(request, cat_slug):
 
 def archive(request, year):
     if year > 2024:
-        uri = reverse('cats', args=('music'))
-        return redirect(uri)
+        uri = reverse('cats', args=('sport', ))
+        return HttpResponsePermanentRedirect(uri)
+
 
     return HttpResponse(f'<h1>Архив по годам</h1><p>{year}</p>')
 
