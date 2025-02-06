70c72505b751dcbd2d4f93974fc1c31cd69af879
SablezubRabbit <yurinv@mail.ru>
2025-02-06 15:40:17 +0300

    Add url

--- women/views.py
+++ women/views.py
@@ -2,27 +2,49 @@ from django.http import HttpResponse, HttpResponseNotFound, Http404, HttpRespons
 from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.views.defaults import page_not_found
+from django.template.loader import render_to_string
 
+
+menu = [
+    {'title': "О нас", 'url_name': 'about'},
+    {'title': "Добавить статью", 'url_name': 'add_page'},
+    {'title': "Обратная связь", 'url_name': 'contact'},
+    {'title': "Войти", 'url_name': 'login'},
+]
+
+data_db = [
+    {'id': 1, 'title': 'Анджелина Джоли', 'content': 'Биография Анджелина Джоли', 'is_published': True},
+    {'id': 2, 'title': 'Марго Робби', 'content': 'Биография Марго Робби', 'is_published': False},
+    {'id': 3, 'title': 'Джулия Робертс', 'content': 'Биография Джулия Робертс', 'is_published': True},
+]
 
 def index(request):
-    return HttpResponse("Страница приложения women.")
+    # t = render_to_string('women/index.html')
+    # return HttpResponse(t)
+    data = {
+        'title': 'Главная страница',
+        'menu': menu,
+        'posts': data_db,
+    }
+    return render(request, 'women/index.html', context=data)
 
 
-def categories(request, cat_id):
-    return HttpResponse(f'<h1>Статьи по категориям</h1><p>id: {cat_id}</p>')
+def about(request):
+    return render(request, 'women/about.html', {'title': 'О сайте', 'menu': menu})
 
 
-def categories_by_slug(request, cat_slug):
-    return HttpResponse(f'<h1>Статьи по категориям</h1><p>slug: {cat_slug}</p>')
+def show_post(request, post_id):
+    return HttpResponse(f'Отображение статьи с id = {post_id}')
 
 
-def archive(request, year):
-    if year > 2024:
-        uri = reverse('cats', args=('sport', ))
-        return HttpResponsePermanentRedirect(uri)
+def addpage(request):
+    return HttpResponse('Добавление статьи')
 
+def contact(request):
+    return HttpResponse('Обратная связь')
 
-    return HttpResponse(f'<h1>Архив по годам</h1><p>{year}</p>')
+def login(request):
+    return HttpResponse('Авторизация')
 
 def page_not_found(request, exception):
     return HttpResponseNotFound('<h2>Страница не найдена</h2>')
