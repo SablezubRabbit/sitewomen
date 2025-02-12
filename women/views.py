6d3b3f671caa9c99fa5a0af458cb00dee8ab44e8
SablezubRabbit <yurinv@mail.ru>
2025-02-12 10:29:31 +0300

    Add categories

--- women/views.py
+++ women/views.py
@@ -4,7 +4,7 @@ from django.urls import reverse
 from django.views.defaults import page_not_found
 from django.template.loader import render_to_string
 
-from .models import Women
+from .models import Women, Category
 
 menu = [
     {'title': "О нас", 'url_name': 'about'},
@@ -21,11 +21,6 @@ data_db = [
     {'id': 3, 'title': 'Джулия Робертс', 'content': 'Биография Джулия Робертс', 'is_published': True},
 ]
 
-cats_db = [
-    {'id':1, 'name': 'Актрисы'},
-    {'id':2, 'name': 'Певицы'},
-    {'id':3, 'name': 'Спортсменки'},
-]
 
 def index(request):
     posts = Women.published.all()
@@ -64,12 +59,15 @@ def contact(request):
 def login(request):
     return HttpResponse('Авторизация')
 
-def show_category(request, cat_id):
+def show_category(request, cat_slug):
+    category = get_object_or_404(Category, slug=cat_slug)
+    posts = Women.published.filter(cat_id=category.pk)
+
     data = {
-        'title': 'Отображение по рубрикам',
+        'title': f'Рубрика: {category.name}',
         'menu': menu,
-        'posts': data_db,
-        'cat_selected': cat_id,
+        'posts': posts,
+        'cat_selected': category.pk,
     }
     return render(request, 'women/index.html', context=data)
 
